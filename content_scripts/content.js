3cb393f9b06ff2b03ee442328084e223a858d25e
Marcin Warpechowski <warpech@gmail.com>
2017-08-04 14:11:27 +0200

    make the button actually closing the issues

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,3 +1,5 @@
+//I know the code is a mess :)
+
 if (typeof browser == "undefined") {
     var browser = chrome;
 }
@@ -6,25 +8,23 @@ function findCommentButton(parent) {
     return parent.querySelector("#partial-new-comment-form-actions > button.btn.btn-primary");
 }
 
+function findCloseButton(parent) {
+    return parent.querySelector("#partial-new-comment-form-actions > button[name='comment_and_close']");
+}
+
 function findCommentTextarea(parent) {
     return parent.querySelector("#new_comment_field");
 }
 
 var replies = [{
-        title: "I think this issue can be closed.",
-        closes: false,
-        issue: true,
-        pr: false
-    },
-    {
         title: "I think this issue can be closed. The problem is solved.",
-        closes: false,
+        closes: true,
         issue: true,
         pr: false
     },
     {
         title: "I think this issue can be closed. All of the items are tracked in other issues.",
-        closes: false,
+        closes: true,
         issue: true,
         pr: false
     },
@@ -81,12 +81,13 @@ function createButton(doc) {
 
 function createMenu(doc) {
     var elem = doc.createElement("div");
+    elem.id = "quick-reply-container";
     elem.style.float = "right";
     elem.style.marginRight = "5px";
 
     elem.innerHTML = `<div style="position: relative; clear: both">
-    <div class="select-menu-modal-holder">
-        <div class="select-menu-modal subscription-menu-modal js-menu-content">
+    <div class="select-menu-modal-holder" style="bottom: 0">
+        <div class="select-menu-modal subscription-menu-modal js-menu-content" style="margin-bottom: 0">
           <div class="select-menu-header js-navigation-enable" tabindex="-1">
             <svg aria-label="Close" class="octicon octicon-x js-menu-close" height="16" role="img" version="1.1" viewBox="0 0 12 16" width="12"><path fill-rule="evenodd" d="M7.48 8l3.75 3.75-1.48 1.48L6 9.48l-3.75 3.75-1.48-1.48L4.52 8 .77 4.25l1.48-1.48L6 6.52l3.75-3.75 1.48 1.48z"></path></svg>
             <span class="select-menu-title">Choose an option to send a quick reply</span>
@@ -104,7 +105,7 @@ function createMenu(doc) {
     });
 
     var quickReplyButton = createButton(document);
-    elem.insertBefore(quickReplyButton, elem.firstChild);
+    elem.appendChild(quickReplyButton);
 
     quickReplyButton.addEventListener("click", () => {
         elem.querySelector('.select-menu-modal-holder').style.display = "block";
@@ -125,13 +126,21 @@ function createMenu(doc) {
     return elem;
 }
 
-function createOption(doc, text) {
+function createOption(doc, reply) {
     var elem = doc.createElement("div");
     elem.classList.add("select-menu-item", "js-navigation-item");
     elem.setAttribute("role", "menuitem");
     elem.setAttribute("tabindex", "0");
 
+    var text = reply.title;
+    var icon = "";
+
+    if (reply.closes) {
+        icon = `<span class="select-menu-item-icon" title="Closes the issue">🔴</span>`;
+    }
+
     elem.innerHTML = `<div class="select-menu-item-text">
+      ${icon}
       <span class="select-menu-item-text">${text}</span>
     </div>`;
 
@@ -139,18 +148,33 @@ function createOption(doc, text) {
 }
 
 function init(ev) {
-    createStyle(document);
-
     var commentButton = findCommentButton(document.body);
     if (commentButton) {
+        var container = document.querySelector("#quick-reply-container");
+        if (container) {
+            var quickReplyButton = container.querySelector(".btn");
+            if (commentButton.hasAttribute("disabled")) {
+                quickReplyButton.setAttribute("disabled", "");
+            } else {
+                quickReplyButton.removeAttribute("disabled");
+            }
+            return;
+        }
+
         var menu = createMenu(document);
         var menuItems = menu.querySelector("[role='menu']");
         replies.forEach((reply) => {
-            var item = createOption(document, reply.title);
+            var item = createOption(document, reply);
             item.addEventListener("click", () => {
                 var textarea = findCommentTextarea(document.body);
                 textarea.innerHTML = "Quick Reply: " + reply.title;
                 menu.close();
+                if (reply.closes) {
+                    findCloseButton(document.body).click();
+                } else {
+                    commentButton.click();
+                }
+                textarea.innerHTML = "";
             });
             menuItems.appendChild(item);
         });
@@ -158,4 +182,15 @@ function init(ev) {
     }
 }
 
+var target = document.querySelector('.discussion-timeline');
+var observer = new MutationObserver(function(mutations) {
+    init();
+});
+var config = {
+    childList: true,
+    subtree: true
+};
+observer.observe(target, config);
 init();
+
+createStyle(document);
